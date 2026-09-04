194e11630249424d4e4bc85d7569b95c20a6ffaf
DESKTOP-17CSARF\SCT <2506513756@qq.com>
2026-09-04 17:35:12 +0800

    remove commented codes

--- plc_server.py
+++ plc_server.py
@@ -95,21 +95,12 @@ async def lifespan(app: FastAPI):
         print("【lifespan】物理通道已建立")
 
         polling_job=asyncio.create_task(plc_polling_task())
-        # 不等，先直接异步执行下面代码了，所以global_plc_cache为空
-        # storage_job=asyncio.create_task(influx_storage_task())
-        # 本地远程切换，
-        # await write_single_reg(0,2)
-        # 手动自动切换
-        # await write_single_reg(399,2)
-        # print('已切换为远程和手动')
-
         yield
 
     finally:
         # 关闭：断开 PLC 连接
         print("正在断开 PLC 连接...")
         polling_job.cancel()
-        # storage_job.cancel()
 
         try:
             await asyncio.gather(polling_job)
@@ -131,45 +122,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-# === 验证示例 ===
-# 假设 PLC 内部真实的浮点数是 50.5
-# 西门子 PLC 中读取出来的两个 16 位十进制整数分别为：16946 和 0
-# reg1 = 16946
-# reg2 = 0
-
-# result = siemens_registers_to_float(reg1, reg2)
-# print(f"解析西门子浮点数结果: {result}")  # 输出: 50.5
-
-
-# ==================== PLC 异步采集任务 ====================
-# async def plc_polling_task():
-#     """该任务在后台独立运行，有且仅有它一个人维持与 PLC 的长连接"""
-#     # global global_plc_cache, global_display_temp_cache, global_humid_cache
-#     while True:
-#         if not plc_client.connected:
-#             print("【连接断开，等待自动重连】")
-#             await asyncio.sleep(3)
-#             continue
-#         try:
-            
-#             app.state.global_plc_cache=await partial_read(35,120)
-#             # print("第1分片读取成功")
-#             # print(f"【采集成功】温度数据: {global_plc_cache[0]} | 时间: {datetime.now()}")
-    
-#             # break
-#             app.state.global_plc_cache.extend(await partial_read(155,20))
-#             app.state.global_display_temp_cache = [round(x / 10, 1) for x in app.state.global_plc_cache]
-
-#             # 读取140个湿度数据
-#             app.state.global_humid_cache.extend(await partial_read(175,120))
-#             app.state.global_humid_cache.extend(await partial_read(195,20))
-#         except Exception as e:
-#             print(f"【采集异常】: {e}")
-            
-#         # 这里控制采集频率：每 （1秒）高频采集一次
-#         await asyncio.sleep(1)
-
 
 async def plc_polling_task():
     """该任务在后台独立运行，有且仅有它一个人维持与 PLC 的长连接"""
@@ -289,75 +241,6 @@ async def prep_store_data_cache(data_cache, table, field_prefix):
         raise Exception(f'##############prep_store_data_cache 发生异常: {e}')
 
 
-# async def build_payload_str( table, field_prefix):
-#     # 内存中不一定会立即有数据，需要判断
-#     try:
-#         if not data_cache:
-#             await asyncio.sleep(2)
-#             print('Error: data_cache is null, check next round')
-#             return ''
-#         plc_channels={}
-#         for i in range(0,140):
-#             plc_channels[f"{field_prefix}{i}"] = data_cache[i]
-#             # print(global_plc_cache[i])
-#         device_tags = {
-#             "plc_type": "s7-smart200",
-#             "station_id": "line_01"
-#         }
-
-#         # 动态生成 140 个字段的行协议数据
-#         influx_data_line = await build_influx_line_protocol(
-#             measurement = table, 
-#             tags = device_tags, 
-#             fields=plc_channels
-#         )
-        
-#         print(f'拼接后的字符串： {influx_data_line}')
-#         return influx_data_line
-#     except Exception as e:
-#         raise Exception('##############build_payload_str 发生异常',e)
-
-
-# todo: seperate as async minor tasks for temp and humid data storage
-# async def influx_storage_task():
-#     # ==================== 3. 核心循环采集 ====================
-#     try:
-#         print("正在启动上位机采集与存储服务...")
-#         # print(global_plc_cache)
-        
-#         # plc_channels={}
-#         # if not global_plc_cache:
-#         #     await asyncio.sleep(2)
-#         # if not global_plc_cache:
-#         #     print('Error global_plc_cache is null, to return')
-#         #     return
-        
-#         while True:
-#             try:
-#                 # shouldn't be waiting here for 5mins. as it would cause long waiting
-#                 # await asyncio.sleep(STORAGE_INTERVAL)
-#                 influx_data_line = await build_payload_str(app.state.global_plc_cache,'plc_temp_data','temp')
-#                 if not influx_data_line:
-#                     continue
-#                 await send_to_influx(influx_data_line)
-#                 print('【温度数据存储成功】')
-#                 # print('一次性退出')
-#                 # break
-                
-#                 influx_data_line = await build_payload_str(app.state.global_humid_cache,'plc_humid_data','humid')
-#                 if not influx_data_line:
-#                     continue
-#                 await send_to_influx(influx_data_line)
-#                 print('【湿度数据存储成功】')
-#             except Exception as e:
-#                 print('【ERROR：】存储数据发生异常',e)
-#             #  每隔5 min存储一次
-#             await asyncio.sleep(STORAGE_INTERVAL0)
-#     except KeyboardInterrupt:
-#         print("\n程序已手动停止。")
-#         # 关闭连接，释放资源
-#         plc_client.close()
-
 async def send_to_influx(payload_text: str):
     """
     底层的异步发送函数，向 InfluxDB 发送 HTTP POST 请求
@@ -391,8 +274,5 @@ async def write_single_reg(start_add: int, val:int):
         print("写入成功，当前寄存器值:", response)        
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser(description="智慧粮仓系统启动脚本")
-    
-
     # 核心：启动内置 Web 容器，监听 0.0.0.0 允许局域网（手机）访问
     uvicorn.run("plc_server:app", host="0.0.0.0", port=8000, reload=True)
