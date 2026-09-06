25eab8d946060d7459a54664f6d54377a03683ad
unknown <fayed@qq.com>
2026-09-06 11:14:09 +0800

    test logger

--- test/test_join.py
+++ test/test_join.py
@@ -1,9 +1,11 @@
 # for i in range(140):
 #     print(f'temp{i},')
 
-
+from log.plc_logger import get_logger
 
 temp_cols = [f"temp{i}" for i in range(120,140)]
 temp_all_cols = ", ".join(temp_cols)    
 
-print(temp_all_cols)
+logger=get_logger();
+
+logger.info(temp_all_cols)
